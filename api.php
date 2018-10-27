bbe719b356e7b4d9f0f07ca6388711e2044ceb5a
user <user@192-168-0-177.rdsnet.ro>
2018-10-27 16:02:21 +0300

    hmm

--- api.php
+++ api.php
@@ -1,8 +1,10 @@
 <?php
 
-error_reporting(E_ERROR);
+error_reporting(E_ALL);
 ini_set('display_startup_errors', 1);
-//ini_set('display_errors', 1);
+ini_set("log_errors", 1);
+ini_set("error_log", "./php-error.log");
+ini_set('display_errors', 0);
 
 
 $app_id = 'c9-test-1wvq8n7g';
